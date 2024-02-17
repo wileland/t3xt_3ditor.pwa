5c96a3797fdfeb5accaf13d77ae72ba40746deb4
wileland <wileland7@gmail.com>
2024-02-16 23:52:16 -0600

    Update install.js to use single quotes and improve UI

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,25 +1,34 @@
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall');
 
 let deferredPrompt;
 
-window.addEventListener("beforeinstallprompt", (event) => {
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the mini-infobar from appearing on mobile
   event.preventDefault();
+  // Stash the event so it can be triggered later.
   deferredPrompt = event;
-  butInstall.style.display = "block";
+  // Update UI notify the user they can install the PWA
+  butInstall.style.display = 'block';
 });
 
-butInstall.addEventListener("click", async () => {
-  butInstall.style.display = "none";
-
+butInstall.addEventListener('click', async () => {
+  // Hide the app provided install promotion
+  butInstall.style.display = 'none';
+  // Show the install prompt
   if (deferredPrompt) {
     deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
     const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
     console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
     deferredPrompt = null;
   }
 });
 
-window.addEventListener("appinstalled", (event) => {
+window.addEventListener('appinstalled', (event) => {
+  // Clear the deferredPrompt so it can be garbage collected
   deferredPrompt = null;
-  console.log("PWA was installed", event);
+  // Optionally, send analytics to indicate successful install
+  console.log('PWA was installed', event);
 });
